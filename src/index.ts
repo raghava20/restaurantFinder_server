3714a1aaf64bd7f5d5aa09d663087e7d639a3494
raghava20 <raghavaanand22@gmail.com>
2022-04-21 09:13:51 +0530

    setup express server

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import { restaurantsRouter } from "./routers/Restaurants.route";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4001;
+
+app.use(express.json());
+app.use(morgan("tiny"));
+
+app.get("/", (req, res) => {
+  res.send("Hello You out there!");
+});
+
+app.use("/api/restaurants", restaurantsRouter);
+
+app.listen(PORT, () => console.log("listening on port ", PORT));
