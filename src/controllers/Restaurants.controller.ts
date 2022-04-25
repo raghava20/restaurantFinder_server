8860917d341136072f1237a5deab64613d8244c6
raghava20 <raghavaanand22@gmail.com>
2022-04-25 14:14:08 +0530

    added review endpoint

--- src/controllers/Restaurants.controller.ts
+++ src/controllers/Restaurants.controller.ts
@@ -3,7 +3,12 @@ import db from "../db";
 
 export const getAllRestaurants = async (req: Request, res: Response) => {
   try {
-    const results = await db.query("SELECT * FROM restaurants");
+    // const results = await db.query("SELECT * FROM restaurants");
+    const results = await db.query(
+      `SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as avg_rating
+       FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id`
+    );
+    console.log(results);
     res.json({
       results: results.rows.length,
       data: results.rows,
@@ -14,13 +19,20 @@ export const getAllRestaurants = async (req: Request, res: Response) => {
 };
 
 export const getOneRestaurant = async (req: Request, res: Response) => {
-  const { restaurantsid } = req.params;
+  const { restaurantid } = req.params;
 
   try {
-    const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [
-      restaurantsid,
-    ]);
-    res.json({ data: results.rows[0] });
+    const restaurant = await db.query(
+      `SELECT * FROM restaurants LEFT JOIN (SELECT restaurant_id, COUNT(*), TRUNC(AVG(rating),1) as avg_rating
+      FROM reviews GROUP BY restaurant_id) reviews ON restaurants.id = reviews.restaurant_id WHERE id = $1`,
+      [restaurantid]
+    );
+    const reviews = await db.query(
+      "SELECT * FROM reviews WHERE restaurant_id = $1",
+      [restaurantid]
+    );
+    console.log(reviews);
+    res.json({ restaurant: restaurant.rows[0], review: reviews.rows });
   } catch (err) {
     console.log(err);
   }
@@ -42,11 +54,11 @@ export const createRestaurant = async (req: Request, res: Response) => {
 
 export const updateOneRestaurant = async (req: Request, res: Response) => {
   const { name, location, price_range } = req.body;
-  const { restaurantsid } = req.params;
+  const { restaurantid } = req.params;
   try {
     const results = await db.query(
       "UPDATE restaurants SET name = $1,location = $2, price_range = $3 where id = $4 RETURNING *",
-      [name, location, price_range, restaurantsid]
+      [name, location, price_range, restaurantid]
     );
     res.json({ data: results.rows[0] });
   } catch (err) {
@@ -55,12 +67,27 @@ export const updateOneRestaurant = async (req: Request, res: Response) => {
 };
 
 export const deleteOneRestaurant = async (req: Request, res: Response) => {
-  const { restaurantsid } = req.params;
+  const { restaurantid } = req.params;
+  console.log(restaurantid);
   try {
-    const results = await db.query("DELETE * FROM restaurants WHERE id = $1", [
-      restaurantsid,
+    const results = await db.query("DELETE FROM restaurants WHERE id = $1", [
+      restaurantid,
     ]);
-    res.json({ data: results.rows });
+    res.json({ result: "Success" });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const createReview = async (req: Request, res: Response) => {
+  const { restaurantid } = req.params;
+  const { name, review, rating } = req.body;
+  try {
+    const results = await db.query(
+      "INSERT INTO reviews (restaurant_id,name,review,rating) values($1,$2,$3,$4) RETURNING *",
+      [restaurantid, name, review, rating]
+    );
+    res.json({ data: results.rows[0] });
   } catch (err) {
     console.log(err);
   }
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import { restaurantsRouter } from "./routers/Restaurants.route";
 import morgan from "morgan";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4001;
 
+app.use(cors());
 app.use(express.json());
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello You out there!");
